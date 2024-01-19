7a87025e035c44c61aa20f7fdc543d927962a14f
alice <castaldialicee@gmail.com>
2024-01-19 15:42:18 +0100

    Aggiungere i file di progetto.

---
+++ es compito2/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_compito2
+{
+    internal class Persona
+    {
+        public DateTime Datadinascita {  get; set; }
+        public int Calcolaeta()
+        {
+           DateTime oggi= DateTime.Today;
+            
+            
+            if (oggi.Day>=Datadinascita.Day)
+            
+               return (oggi.Year - Datadinascita.Year); 
+            
+            else
+           
+                return oggi.Year - Datadinascita.Year-1;
+            
+
+        }
+    }
+}
---
+++ es compito2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_compito2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            Console.WriteLine("inserire data di nascita:");
+            persona.Datadinascita=DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("ha {0} anni", persona.Calcolaeta());
+            Console.ReadLine();
+        }
+    }
+}
